5dc62b3f4a982553b722b2fdf99e823d55151f3b
mark <murimimg180@gmail.com>
2018-05-11 19:21:39 +0300

    made tests and models

--- instagram/gallery/tests.py
+++ instagram/gallery/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from .models import Editor,Post
 # Create your tests here.
+
+class EdtitorTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.mark= Editor(username = '_illest.niccur',editor_name = 'mark', email = 'murimimg180@gmail.com')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.mark,Editor))
