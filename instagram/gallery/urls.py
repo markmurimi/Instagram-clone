658e5c624c3f0830212c87f980d3a126b8fcc6bb
mark <murimimg180@gmail.com>
2018-05-11 16:55:13 +0300

    made changes

--- instagram/gallery/urls.py
+++ instagram/gallery/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url,include
 from . import views
 
 urlpatterns=[
-    url('^',views.welcome,name = 'welcome'),
+    url('^$',views.welcome,name = 'welcome'),
     url(r'^profile/$',views.profile,name= 'profile'),
 ]
