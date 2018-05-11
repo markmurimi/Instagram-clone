e6213900dd88383a0aa5a27301c4dd154134db4f
mark <murimimg180@gmail.com>
2018-05-11 08:56:22 +0300

    made changes

--- instagram/gallery/models.py
+++ instagram/gallery/models.py
@@ -1,8 +1,4 @@
 from django.db import models
 
 # Create your models here.
-class Editor(models.Model):
-    first_name = models.CharField(max_length =30)
-    last_name = models.CharField(max_length =30)
-    email = models.EmailField()
-    username = models.CharField(max_length =30)
+class Post(models.Model):
