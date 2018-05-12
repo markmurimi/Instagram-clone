afa8f945925e029b8dfd92bd4a7230bd1ecb690c
mark <murimimg180@gmail.com>
2018-05-12 12:48:39 +0300

    made changes

--- instagram/gallery/tests.py
+++ instagram/gallery/tests.py
@@ -16,3 +16,17 @@ class EdtitorTestClass(TestCase):
         self.mark.save_editor()
         editors = Editor.objects.all()
         self.assertTrue(len(editors) > 0)
+
+class PostTestClass(TestCase):
+    def setUp(self):
+        # creating an editor and saving  it
+        self.mark= Editor(username = '_illest.niccur',editor_name = 'mark', email = 'murimimg180@gmail.com')
+        self.mark.save_editor()
+
+        # creating a new post and saving it
+        self.new_post= Post(post_name = 'Test Post', post = 'This is a test', editor = self.mark)
+        self.new_post.save()
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        Post.objects.all().delete()
